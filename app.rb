1fe7e70fe0a09d8414d50f551cbe8e34ca779da4
Samir Undavia <samir1@brandeis.edu>
2015-03-13 20:29:42 -0400

    Merge branch 'master' of https://github.com/samir1/nanoTwitter
    Conflicts:
    	app.rb
    	views/layout.erb

--- app.rb
+++ app.rb
@@ -1,6 +1,14 @@
-<<<<<<< HEAD
 require 'rubygems'
 require 'sinatra'
+require 'active_record'
+require './models/user'
+# require 'sinatra/active_record'
+
+
+ActiveRecord::Base.establish_connection(
+  :adapter => 'sqlite3',
+  :database =>  'db/sinatra_application.sqlite3.db'
+)
 
 configure do
   enable :sessions
@@ -43,58 +51,3 @@ end
 get '/secure/timeline' do
   erb :timeline
 end
-=======
-require 'rubygems'
-require 'sinatra'
-require 'active_record'
-require './models/user'
-# require 'sinatra/active_record'
-
-
-ActiveRecord::Base.establish_connection(
-  :adapter => 'sqlite3',
-  :database =>  'db/sinatra_application.sqlite3.db'
-)
-
-configure do
-  enable :sessions
-end
-
-helpers do
-  def username
-    session[:identity] ? session[:identity] : 'Login'
-  end
-end
-
-before '/secure/*' do
-  if !session[:identity] then
-    session[:previous_url] = request.path
-    @error = 'Must be logged into to visit ' + request.path
-    halt erb(:login_form)
-  end
-end
-
-get '/' do
-  erb :main
-end
-
-get '/login/form' do 
-  erb :login_form
-end
-
-post '/login/attempt' do
-  session[:identity] = params['username']
-  where_user_came_from = session[:previous_url] || '/'
-  redirect to where_user_came_from 
-end
-
-get '/logout' do
-  session.delete(:identity)
-  erb "<div class='alert alert-message'>Logged out</div>"
-end
-
-
-get '/secure/place' do
-  erb "Only <%=session[:identity]%> can access this page"
-end
->>>>>>> 5d4ca169138d61bed97aa23ff004558f2f43bb71
