a010e761f2c72cc6c7790e141b6247af36f95e24
Samir <samir1@brandeis.edu>
2015-04-25 18:21:47 -0400

    change sql queries to activerecord

--- models/tweet.rb
+++ models/tweet.rb
@@ -1,7 +1,9 @@
 class Tweet < ActiveRecord::Base
     # validates_uniqueness_of :name, :email
-        validates :text, uniqueness: { scope: :owner, 
-    message: "cant write the same tweet twice" }
+    validates :text, uniqueness: { scope: :owner, message: "cant write the same tweet twice" }
+
+    belongs_to :user
+
     def to_json
         super()
     end
--- models/user.rb
+++ models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
     validates_uniqueness_of :username, :email
+
+    has_many :tweets
 end
 
 
